3f1cb9cc772ad430371fe8d3c724692781e1d27e
NguyenTuanMinh <minhnguyen29bh@gmail.com>
2026-04-21 17:40:24 +0700

    Add files via upload

---
+++ Bài 3/Bài 3.sql
@@ -0,0 +1,57 @@
+use session06;
+
+
+create table users(
+	user_id int primary key auto_increment,
+    user_name Nvarchar(50) not null
+);
+
+INSERT INTO users (user_name) 
+VALUES 
+('Nguyễn Tuấn Minh'), 
+('Nguyễn Duy Khánh');
+
+create table booking(
+	user_id int,
+	booking_id int primary key auto_increment,
+    `status` varchar(20),
+    total_room int check (total_room > 0)
+);
+
+INSERT INTO booking (user_id, `status`, total_room) 
+VALUES 
+(2, 'BOOKED', 1), 
+(2, 'BOOKED', 1), 
+(2, 'BOOKED', 1), 
+(2, 'BOOKED', 1), 
+(2, 'BOOKED', 1),
+(2, 'CANCELLED', 1), 
+(2, 'CANCELLED', 1), 
+(2, 'CANCELLED', 1), 
+(2, 'CANCELLED', 1), 
+(2, 'CANCELLED', 1), 
+(2, 'CANCELLED', 1),
+(1, 'BOOKED', 1), 
+(1, 'BOOKED', 1), 
+(1, 'BOOKED', 1), 
+(1, 'BOOKED', 1);
+
+delete from booking;
+
+select * from booking;
+
+select u.user_id, u.user_name, count(b.user_id) as 'datphong',
+	SUM(
+		case 
+			when b.`status` = 'CANCELLED' then 1 
+            else 0
+		end  
+    ) as 'Số lần hủy'
+from booking b
+JOIN users u ON b.user_id = u.user_id
+group by u.user_id, u.user_name
+having count(b.user_id) >= 10 and sum(case when b.`status` = 'CANCELLED' then 1 else 0 end);
+
+
+
+
